a8fc7b9dae875d6c16cd68eb8a3694b01f88208b
PRATHAMESH <84615230+prathamvs@users.noreply.github.com>
2025-07-01 08:46:29 +0530

    Update information_retrieval.py

--- backend/information_retrieval.py
+++ backend/information_retrieval.py
@@ -16,9 +16,9 @@ from langchain_core.prompts import PromptTemplate
 
 # Global OpenAI configuration for Azure integration
 openai.api_type = "azure"
-openai.api_base = "https://apim-guardian-prv-fc.aihub.se.com"
-openai.api_version = "2024-06-01"
-openai.api_key = "b74bf34f88b449f5b25764e363d4dd49"
+openai.api_base = ""
+openai.api_version = ""
+openai.api_key = ""
 
 class InformationRetrievalProcessor:
     """
